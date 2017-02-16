89287501b253f5db4a3205820a0352a51fc5dd8e
Muru <Murugasundaram_Annamalai@cable.comcast.com>
2017-02-16 15:24:32 -0500

    Removed Target folders

--- target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
---DROP TABLE IF EXISTS patient_diet_order;
-CREATE TABLE IF NOT EXISTS patient_diet_order
-(
-  id            CHAR (36) PRIMARY KEY NOT NULL,
-  created_at    BIGINT(40),
-  last_modified BIGINT(40),
-  value         VARCHAR(255)
-);
